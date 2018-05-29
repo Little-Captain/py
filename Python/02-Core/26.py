1d8db60666d8bb700f2b75cd04c9860d09f90870
Little-Captain <littlecaptain@foxmail.com>
2018-05-29 23:30:12 +0800

    断点调试学习

---
+++ Python/02-Core/26.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# pdb: python debug
+
+# break	    b	设置断点
+# continue	c	继续执行程序
+# list	    l	查看当前行的代码段
+# step	    s	进入函数
+# return	r	执行代码直到从当前函数返回
+# quit	    q	中止并退出
+# next	    n	执行下一行
+# print	    p	打印变量的值
+# help	    h	帮助
+# args	    a	查看传入参数
+# 回车	        重复上一条命令
+# break	    b	显示所有断点
+# break lineno	    b lineno	    在指定行设置断点
+# break file:lineno	b file:lineno	在指定文件的行设置断点
+# clear num		删除指定断点
+# bt		    查看函数调用栈帧
+
